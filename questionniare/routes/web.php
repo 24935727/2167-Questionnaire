ffb450400c5896f7cc31c04df83ef7e26cc4639e
24935727 <114491185+24935727@users.noreply.github.com>
2023-05-01 08:55:03 +0100

    added comments

--- questionniare/routes/web.php
+++ questionniare/routes/web.php
@@ -18,14 +18,15 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
-
+// Routes for the questionnaire controller including methods create, store, show and update 
 Route::get('/questionnaire/create', 'QuestionnaireController@create');
 Route::post('/questionnaires', 'QuestionnaireController@store')->name('questionnaire.store');
 Route::get('/questionnaires/{questionnaire}', 'QuestionnaireController@show')->name('questionnaire.show');
 Route::patch('questionnaires/status/{questionnaire}', 'QuestionnaireController@update');
-
+// Routes for the question controller including methdods create and store
 Route::get('/questionnaires/{questionnaire}/questions/create', 'QuestionController@create')->name('questions.create');
 Route::post('/questionnaires/{questionnaire}/questions', 'QuestionController@store')->name('questions.store');
+// Routes for the response controller including methods create, show, store and destroy
 
 Route::get('/questionnaires/{questionnaire}/responses/create', 'ResponseController@create')->name('responses.create');
 Route::get('/questionnaires/responses/show', 'ResponseController@show')->name('responses.show');
